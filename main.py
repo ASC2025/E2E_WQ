110adeaf72d57a68920305a6fae459f4e56659ed
asc2025 <a.ali201317@gmail.com>
2025-07-16 20:13:04 +0300

    02-validation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 from E2E_WQ_ML_Project import logger
 from E2E_WQ_ML_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from E2E_WQ_ML_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from E2E_WQ_ML_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 #from E2E_WQ_ML_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 #from E2E_WQ_ML_Project.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from E2E_WQ_ML_Project.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -14,6 +14,17 @@ STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/E2E_WQ_ML_Project/config/configuration.py
+++ src/E2E_WQ_ML_Project/config/configuration.py
@@ -1,6 +1,8 @@
 from E2E_WQ_ML_Project.constants import *
 from E2E_WQ_ML_Project.utils.common import read_yaml, create_directories
-from E2E_WQ_ML_Project.entity.config_entity import (DataIngestionConfig)
+from E2E_WQ_ML_Project.entity.config_entity import (DataIngestionConfig,
+                                                    DataValidationConfig)  
+
 
 class ConfigurationManager:
     def __init__(
@@ -30,3 +32,19 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
